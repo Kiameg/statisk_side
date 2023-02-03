422db91497e2c86511b7eb4d93ad73a8842bdee2
Kia Meggele <kia1262@stud.kea.dk>
2023-02-03 13:41:42 +0100

    js

--- product.js
+++ product.js
@@ -1,1 +1,22 @@
-console.log("hej");
+const id = 1532;
+const url = `https://kea-alt-del.dk/t7/api/products//${id}`;
+const imagePath = `https://kea-alt-del.dk/t7/images/webp/640//${id}.webp`;
+
+function fetchData() {
+  fetch(url)
+    .then((res) => res.json())
+    .then(showProduct);
+}
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector("#model").textContent = product.productdisplayname;
+  document.querySelector("img").src = imagePath;
+  document.querySelector("#color").textContent = product.basecolour;
+  document.querySelector("#id").textContent = product.id;
+  document.querySelector("h3").textContent = product.productdisplayname;
+  document.querySelector("#brand").textContent = product.brandname;
+  document.querySelector("#type").textContent = product.articletype;
+}
+
+fetchData();
